dd26d7ac14d754e9946da0c3203765c3a457e985
Radu Chirilov <radulin4a@gmail.com>
2022-03-31 21:39:30 +0300

    New event listeners and dispatches

--- src/main.js
+++ src/main.js
@@ -1,7 +1,41 @@
 // Initial Modal setup
 const modalNode = document.querySelector('.info-modal')
-modalNode.classList.add('disabled-modal')
-//document.querySelector('.modal-wrapper').removeChild(modalNode)
+let currentTimeout = undefined
+let modal_mouseIsOver = false
+let pin_mouseIsOver = false
+
+// Display info-modal at coordinates
+const showModalNode = (x, y) => {
+    modalNode.classList.remove('disabled')
+    modalNode.style.left = `${x}px`
+    modalNode.style.top = `${y}px`
+}
+// Hide info-modal
+const hideModalNode = () => {
+    modalNode.classList.add('disabled')
+}
+
+// Dispatch on info-modal mouse over event
+modalNode.addEventListener('mouseenter', () => {
+    modal_mouseIsOver = true
+})
+// Dispatch on info-modal mouse leave event
+modalNode.addEventListener('mouseleave', () => {
+    modal_mouseIsOver = false
+    // Dispatch event to modalNode
+    const leaveEvent = new Event('modal_leave')
+    modalNode.dispatchEvent(leaveEvent)
+})
+// Add Custom modal_leave Event listener
+const modalLeaveEvent = () => {
+    hideModalNode()
+}
+const addModalLeaveListener = () => {
+    modalNode.addEventListener('modal_leave', modalLeaveEvent)
+}
+const removeModalLeaveListener = () => {
+    modalNode.removeEventListener('modal_leave', modalLeaveEvent)
+}
 
 // Maths and coordinates functions
 const getRelativeCoords = (element) => {
@@ -13,25 +47,37 @@ const getRelativeCoords = (element) => {
     }
 }
 
-/* Map PINs Event listeners */
+
+/* Map PIN Event listeners */
 // On mouse enter event
 const onMouseEnterEvent = (event) => {
+    // Clear listener and timeout from previous Pin
+    removeModalLeaveListener()
+    clearTimeout(currentTimeout)
+    
+    pin_mouseIsOver = true
     const rez = getRelativeCoords(event.target)
-    // console.log(getRelativeCoords(event.target))
-
-    console.log(rez)
-    modalNode.style.left = `${rez.x}px`
-    modalNode.style.top = `${rez.y}px`
-
-    console.log(`Enter: ${event.target.id}`)
+    const { width } = event.target.getBoundingClientRect()
+    showModalNode(rez.x + width / 2, rez.y)
+    //console.log(`Enter Pin: ${event.target.id}`)
 }
 
 // On mouse leave event
 const onMouseLeaveEvent = (event) => {
-    console.log(`Leave: ${event.target.id}`)
+    pin_mouseIsOver = false
+    currentTimeout = setTimeout(() => {
+        // Add event listener or disable modal
+        if(modal_mouseIsOver){
+           addModalLeaveListener()
+        } else {
+            hideModalNode()
+        }
+    }, 1000) // wait a second in order to give the user some breathing space
+    //console.log(`Leave Pin: ${event.target.id}`)
 }
 
-// Set mouseevents listeners for pins
+
+/* Set mouse events listeners for pins */
 const setPinListeners = (pins) => {
     pins.forEach(element => {
         element.addEventListener('mouseenter', onMouseEnterEvent)
@@ -44,9 +90,7 @@ let svgObject = document.querySelector('.eu-map')
 
 svgObject.addEventListener('load', () => {
     svgObject = svgObject.contentDocument
-
+    
     const pinList = svgObject.querySelectorAll('g[id^="Group-6"]')
     setPinListeners(pinList)
-    
-    //console.log(pinList)
 })
